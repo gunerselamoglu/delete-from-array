e526a08e3f7813a7355d5028b8ef1bf513851c82
Güner Selamoğlu <gyuners@hotmail.com>
2022-02-03 11:41:48 +0300

    Delete from array v 1.0.1

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ module.exports= (array, items) => {
   if((array && Array.isArray(array)) && (array && Array.isArray(items))) {
         array.forEach(item => {
          items.includes(item) ? '' : _array.push(item) 
-          
       });   
-      return (_array)  
+      return (_array);
   }
-  
+
   else return('You should use two arrays as parameters');
 }
